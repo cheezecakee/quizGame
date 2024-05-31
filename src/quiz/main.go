4122d2f1c62436fda8f3c4af8dc7fbdcbd8a6b0f
Anabel <anabel.pmoraes@gmail.com>
2024-05-31 05:01:11 -0300

    simple functioning quiz game

---
+++ src/quiz/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+)
+
+type Quiz struct {
+	Question string
+	Answer   string
+}
+
+func askQuestion(q Quiz) bool {
+	var userAnswer string
+	fmt.Printf("Question: %s\n Your Answer: ", q.Question)
+	fmt.Scanln(&userAnswer)
+	return userAnswer == q.Answer
+}
+
+func createQuizList(data [][]string) []Quiz {
+	var quizList []Quiz
+	for i, line := range data {
+		if i >= 0 {
+			var rec Quiz
+			for j, field := range line {
+				if j == 0 {
+					rec.Question = field
+				} else if j == 1 {
+					rec.Answer = field
+				}
+			}
+			quizList = append(quizList, rec)
+		}
+	}
+	return quizList
+}
+
+func main() {
+	// Part 1
+	if len(os.Args) < 2 {
+		fmt.Println("How to run quiz:\n\tQuizt [problems CSV file]")
+		return
+	}
+
+	// Open file
+	path := "./src/quiz/internal/" + os.Args[1]
+
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	// Read csv file
+	csvReader := csv.NewReader(file)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	quizList := createQuizList(data)
+
+	// fmt.Printf("%+v\n", quizList)
+	// Ask user the questions from the files
+	// Compare user answer with file answer
+	// Keep track of how many questions got correct
+	var score int
+	for _, quiz := range quizList {
+		if askQuestion(quiz) {
+			score++
+		}
+	}
+
+	fmt.Printf("You scored %d out of %d.\n", score, len(quizList))
+}
